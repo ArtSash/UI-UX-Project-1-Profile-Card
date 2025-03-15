4a4d893338369aa827d3adcae558f1bd9cef0f8e
Alexander Artamonov <alexander.y.artamonov@gmail.com>
2025-03-16 00:09:06 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const followNumber = document.querySelector('.follow-number');
 const messageButton = document.getElementById('message-button');
 const messageNumber = document.querySelector('.message-number');
 const switchButton = document.querySelector('.switch-button');
+const heartIcon = document.querySelector('.data i');
 
 followButton.addEventListener('click', function() {
   let currentFollow = parseInt(followNumber.textContent);
   followNumber.textContent = currentFollow + 1;
+  heartIcon.classList.add('pulse');
+  setTimeout(() => heartIcon.classList.remove('pulse'), 500);
 });
 
 messageButton.addEventListener('click', function() {
@@ -19,3 +22,4 @@ switchButton.addEventListener('click', function() {
   document.body.classList.toggle('black-theme');
   
 });
+
